a75df6370d6cb236148fdeda1c117ba994f3399d
Ishank Lalwani <ishanklalwani@gmail.com>
2025-01-04 11:26:30 +0530

    add about, contact, footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Contact from './Components/Contact'
+import About from './Components/About'
+import Footer from './Components/Footer'
 
 export default function App() {
   return (
     <div className='h-screen w-full bg-black text-white'>
-         App
+      <About/>
+      <Contact/>
+      <Footer/>
     </div>
   )
 }
